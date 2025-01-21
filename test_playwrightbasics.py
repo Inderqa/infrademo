698d7f9caf104417c64d0d4e19bc63840645caf1
Inderqa <inder093@gmail.com>
2025-01-22 00:13:39 +0530

    updated 1st scenario

--- test_playwrightbasics.py
+++ test_playwrightbasics.py
@@ -2,13 +2,10 @@ import time
 
 from playwright.sync_api import Page
 
-# def test_basic_launch(playwright):
-#     browser = playwright.chromium.launch(headless=False,slow_mo=5000)
-#     context = browser.new_context()
-#     page = context.new_page()
-#     page.goto("https://rahulshettyacademy.com/loginpagePractise")
-
-def test_corelocator(page:Page):
+def test_basic_launch(playwright):
+    browser = playwright.chromium.launch(headless=False,slow_mo=2000)
+    context = browser.new_context()
+    page = context.new_page()
     page.goto("https://rahulshettyacademy.com/loginpagePractise")
     page.get_by_label("Username:").fill("rahulshettyacademy")
     page.get_by_label("Password:").fill("learning")
@@ -16,3 +13,12 @@ def test_corelocator(page:Page):
     page.get_by_role("checkbox",name="terms").click()
     page.get_by_role("button",name="Sign In").click()
     time.sleep(5)
+
+# def test_corelocator(page:Page):
+#     page.goto("https://rahulshettyacademy.com/loginpagePractise")
+#     page.get_by_label("Username:").fill("rahulshettyacademy")
+#     page.get_by_label("Password:").fill("learning")
+#     page.get_by_role("combobox").select_option("teach")
+#     page.get_by_role("checkbox",name="terms").click()
+#     page.get_by_role("button",name="Sign In").click()
+#     time.sleep(5)
